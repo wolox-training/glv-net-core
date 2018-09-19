9f11bc1bca9cb223ad9ddb1343f9d4c54ebd40db
German Lorenz <germangelv@gmail.com>
2018-09-19 18:17:55 -0300

    Repositories in process

--- src/Repositories/MovieRepository.cs
+++ src/Repositories/MovieRepository.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
+<<<<<<< 2b195b81bbc5a1a4ab6ebb48595eebf4f873862b
 using Microsoft.EntityFrameworkCore;
+=======
+>>>>>>> Repositories in process
 using TrainingNet.Models;
 using TrainingNet.Repositories.Database;
 using TrainingNet.Repositories.Interfaces;
@@ -9,6 +12,13 @@ namespace TrainingNet.Repositories
 {
     public class MovieRepository : Repository<Movie>, IMovieRepository
     {
+<<<<<<< 2b195b81bbc5a1a4ab6ebb48595eebf4f873862b
         public MovieRepository(DataBaseContext context) : base(context) {}
+=======
+        public MovieRepository(DataBaseContext context) 
+            : base(context)
+        {
+        }
+>>>>>>> Repositories in process
     }
 }
