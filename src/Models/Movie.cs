369b8bd3cf4d29b47e775629111f7c50f31ab460
German Lorenz <germangelv@gmail.com>
2018-09-20 16:02:25 -0300

    Creado in process Movie.

--- src/Models/Movie.cs
+++ src/Models/Movie.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TrainingNet.Models
 {
@@ -6,8 +8,11 @@ namespace TrainingNet.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
