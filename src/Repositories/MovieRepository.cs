ed2f81efb38cf57d34fb2c58614355275366aa39
German Lorenz <germangelv@gmail.com>
2018-09-28 14:28:57 -0300

    List fix errors between commits

--- src/Repositories/MovieRepository.cs
+++ src/Repositories/MovieRepository.cs
@@ -1,9 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< 2b195b81bbc5a1a4ab6ebb48595eebf4f873862b
 using Microsoft.EntityFrameworkCore;
-=======
->>>>>>> Repositories in process
 using TrainingNet.Models;
 using TrainingNet.Repositories.Database;
 using TrainingNet.Repositories.Interfaces;
@@ -12,13 +9,6 @@ namespace TrainingNet.Repositories
 {
     public class MovieRepository : Repository<Movie>, IMovieRepository
     {
-<<<<<<< 2b195b81bbc5a1a4ab6ebb48595eebf4f873862b
         public MovieRepository(DataBaseContext context) : base(context) {}
-=======
-        public MovieRepository(DataBaseContext context) 
-            : base(context)
-        {
-        }
->>>>>>> Repositories in process
     }
 }
