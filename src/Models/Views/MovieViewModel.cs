0753f24b655d99bb7d00f6203e65e1cfeeed9851
German Lorenz <germangelv@gmail.com>
2018-10-10 17:03:12 -0300

    New property added, missing views

--- src/Models/Views/MovieViewModel.cs
+++ src/Models/Views/MovieViewModel.cs
@@ -11,17 +11,17 @@ namespace TrainingNet.Models.Views
         [Display(Name = "Titulo")]
         public string Title { get; set; }
         
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         
         [Display(Name = "Genero")]
         public string Genre { get; set; }
         
-        [Display(Name = "Precio")]
-        [Column(TypeName = "decimal(18, 2)")]
-        
+        [Display(Name = "Precio"), Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
-    }
+
+        [Display(Name = "Calificación")]
+        public string Rating { get; set; }
+  }
 }
